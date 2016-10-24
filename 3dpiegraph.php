143f87257989b3700c0b6164b10b404d20e46b83
Kapfhe <kapfhamm@hm.edu>
2016-10-24 17:56:30 +0200

    test

--- 3dpiegraph.php
+++ 3dpiegraph.php
@@ -2,67 +2,41 @@
 require_once ('src/jpgraph.php');
 require_once ('src/jpgraph_pie.php');
 require_once ('src/jpgraph_pie3d.php');
-
-
-
-
-
-
-        // Verbindung zur Datenbank erstellen.
-        $db = mysqli_connect("localhost", "root", "", "datenbanken2");
-
-        // Verbindung überprüfen
-        if (mysqli_connect_errno()) {
-            printf("Verbindung fehlgeschlagen: %s\n", mysqli_connect_error());
-            exit();
-        }
-        else{
-
-        }
-        $getAllClubs = mysqli_query($db, "SELECT  Verein, Stimmen
+// Verbindung zur Datenbank erstellen.
+$db = mysqli_connect("localhost", "root", "", "datenbanken2");
+// Verbindung überprüfen
+if (mysqli_connect_errno()) {
+    printf("Verbindung fehlgeschlagen: %s\n", mysqli_connect_error());
+    exit();
+}
+else{
+}
+$getAllClubs = mysqli_query($db, "SELECT  Verein, Stimmen
                                       FROM `fussballvereine` WHERE Stimmen > 0 ");
-        if (!$getAllClubs) {
-            printf("Error: %s\n", mysqli_error($db));
-            exit();
-        }
-        $legend = array();
-        $data = array();
-        while ($zeile = mysqli_fetch_array( $getAllClubs)) {
-            array_push($data,  $zeile['Stimmen']);
-            array_push($legend,  $zeile['Verein']);
-        }
-
-
-
-
-        mysqli_close($db);
-
-
-
-
-
-
-
+if (!$getAllClubs) {
+    printf("Error: %s\n", mysqli_error($db));
+    exit();
+}
+$legend = array();
+$data = array();
+while ($zeile = mysqli_fetch_array( $getAllClubs)) {
+    array_push($data,  $zeile['Stimmen']);
+    array_push($legend,  $zeile['Verein']);
+}
+mysqli_close($db);
 // Some data
 //$data = array(40,60,21,33);
-
 // Create the Pie Graph.
 $graph = new PieGraph(350,250);
-
 $theme_class= new VividTheme;
 $graph->SetTheme($theme_class);
-
 // Set A title for the plot
 $graph->title->Set("Abstimmungsergebnis:");
-
 // Create
 $p1 = new PiePlot3D($data);
 $p1->SetLegends($legend);
 $graph->Add($p1);
-
 $p1->ShowBorder();
 $p1->SetColor('black');
-
 $graph->Stroke();
-
 ?>
--- save.php
+++ save.php
@@ -12,31 +12,24 @@
 
 
 <?php
-
 // Verbindung zur Datenbank erstellen.
 $db = mysqli_connect("localhost", "root", "", "datenbanken2");
-
 // Verbindung überprüfen
 if (mysqli_connect_errno()) {
     printf("Verbindung fehlgeschlagen: %s\n", mysqli_connect_error());
     exit();
-
 }
-
 if (!empty($_GET['voteClub'])) {
     if (empty($club = $_GET['choosenClub'])) {
         echo "<script type='text/javascript'>alert('Bitte Verein auswählen!!!')</script>";
-
     } else {
         mysqli_query($db, "UPDATE fussballvereine SET Stimmen = Stimmen + 1 WHERE Verein = '" . $club . "'");
         $countParticipant = mysqli_query($db, "Select sum(Stimmen) as Anzahl from fussballvereine");
         $zeile = mysqli_fetch_array($countParticipant);
         $count = $zeile['Anzahl'];
         echo "<script type='text/javascript'>alert('Wahl erfolgreich durchgeführt. Es haben bereits ' + $count +' Personen abgestimmt!' )</script>";
-
     }
 }elseif (!empty($_GET['addClub']) && $newClub = $_GET['newClub']) {
-
     $checkQuery = mysqli_query($db, "SELECT * FROM fussballvereine where Verein = '" . $newClub . "'");
     $zeile = mysqli_fetch_array( $checkQuery);
     if ($zeile == ''){
@@ -44,8 +37,6 @@ if (!empty($_GET['voteClub'])) {
     }else{
         echo "<script type='text/javascript'>alert('Verein bereits vorhanden')</script>";
     }
-
-
 }
 mysqli_close($db);
 ?>
@@ -58,7 +49,6 @@ mysqli_close($db);
         <?php
         // Verbindung zur Datenbank erstellen.
         $db = mysqli_connect("localhost", "root", "", "datenbanken2");
-
         // Verbindung überprüfen
         if (mysqli_connect_errno()) {
             printf("Verbindung fehlgeschlagen: %s\n", mysqli_connect_error());
@@ -73,7 +63,6 @@ mysqli_close($db);
             printf("Error: %s\n", mysqli_error($db));
             exit();
         }
-
         while ($zeile = mysqli_fetch_array( $getAllClubs))
         {
             echo "<option value = ". $zeile['Verein'] . "    >". $zeile['Verein'] . " ". $zeile['Stimmen'] . " </option>";
@@ -81,15 +70,15 @@ mysqli_close($db);
         mysqli_close($db);
         ?>
 
-<input type="submit" value = "Abstimmen" />
+        <input type="submit" value = "Abstimmen" />
         <input type = 'hidden' value = 'true' name = 'voteClub'>
 </form>
 
 
 <form action= "">
-<input type = 'hidden' value = 'true' name = 'addClub'>
-<input name = 'newClub' type = 'text' value = ''>
-<input type = 'submit' value = "Hinzufuegen">
+    <input type = 'hidden' value = 'true' name = 'addClub'>
+    <input name = 'newClub' type = 'text' value = ''>
+    <input type = 'submit' value = "Hinzufuegen">
 </form>
 
 
